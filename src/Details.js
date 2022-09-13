993ccbe70813fd28d1ba3ae15a9d26f363584f53
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-09-13 15:18:38 -0600

    Add relevant details to details page

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
+import './Details.css'
 const Details = ({stories , title}) => {
 
 const [story, setStory] = useState([])
@@ -17,11 +18,19 @@ return (
   {story.length ? 
   <>
   <Header />
-  <p className="details-header">{story[0].title}</p>
-  <img src={story[0].multimedia[0].url}/>
+  <p className="details-title">{story[0].title}</p>
+  <p className="byline">{story[0].byline}</p>
+  <div className="details-image-wrapper">
+  <img className="details-image" src={story[0].multimedia[0].url}/>
+  </div>
+  <p className="details-abstract">{story[0].abstract}</p>
+  <p className="cta">See the full story <a href={story[0].url} target='_blank'>here!</a></p>
   </>
   :
+  <>
+  <Header />
   <p>LOADING...</p>
+  </>
 }
   </>
 )
